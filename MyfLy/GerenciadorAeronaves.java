34dda7c989d3748597f9e11d602a6845c595f4bc
MatheusMCosta <37557376+MatheusMCosta@users.noreply.github.com>
2019-10-24 03:07:38 -0300

    Adição de novos métodos na GerenciadorAeronaves

--- MyfLy/GerenciadorAeronaves.java
+++ MyfLy/GerenciadorAeronaves.java
@@ -5,5 +5,26 @@ import java.util.ArrayList;
 
 
 public class GerenciadorAeronaves {
+
+    private ArrayList<Aeronave> avioes;
     
+    public GerenciadorAeronaves(){
+        this.avioes =  new ArrayList<>();
+    }
+    
+    public void adicionar(Aeronave aviao){
+        avioes.add(aviao);
+    }
+    
+     public ArrayList<Aeronave> listarTodas() {
+        return new ArrayList<>(avioes);
+    }
+    
+    public Aeronave buscarCodigo(String codigo) {
+        for(Aeronave a: avioes)
+            if(a.getCodigo().equals(codigo))
+                return a;
+        return null;
+    }
+        
 }
