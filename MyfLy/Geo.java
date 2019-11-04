65222943df16216249437b36cb0889cd86d07046
Jonatasvangroll <jonatas.vangroll@gmail.com>
2019-11-04 16:37:06 -0300

    Update Geo.java

--- MyfLy/Geo.java
+++ MyfLy/Geo.java
@@ -1,15 +1,35 @@
 
 package myfly;
 
-
 public class Geo {
-    
-    private double latitude;
+	private double latitude;
 	private double longitude;
 	
 	public Geo(double latitude, double longitude) {
 		this.latitude = latitude;
 		this.longitude = longitude;
+	}
+
+	public double distancia(Geo p2) {
+	    return distancia(this, p2);
+    }
+
+	public static double distancia(Geo p1, Geo p2) {
+	    double lat1 = Math.toRadians(p1.latitude);
+        double lat2 = Math.toRadians(p2.latitude);
+        double lon1 = Math.toRadians(p1.longitude);
+        double lon2 = Math.toRadians(p2.longitude);
+
+        double diflat = (lat1 - lat2)/2;
+        diflat = Math.sin(diflat) * Math.sin(diflat);
+
+        double diflon = (lon1 - lon2)/2;
+        diflon = Math.sin(diflon) * Math.sin(diflon);
+
+        double aux = diflat + diflon * Math.cos(lat1)*Math.cos(lat2);
+        aux = Math.sqrt(aux);
+
+        return 2*6371 * Math.asin(aux);
 	}
 	
 	public double getLatitude() {
@@ -19,4 +39,10 @@ public class Geo {
 	public double getLongitude() {
 		return longitude;
 	}
+
+	@Override
+	public String toString() {
+		return latitude + ", " + longitude;
+	}
+
 }
