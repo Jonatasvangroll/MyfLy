104d0f7e0ca7bdd6e86236824afe9416e71c02b3
Jonatasvangroll <jonatas.vangroll@gmail.com>
2019-11-04 16:39:44 -0300

    Update GerenciadorCias.java

--- MyfLy/GerenciadorCias.java
+++ MyfLy/GerenciadorCias.java
@@ -5,9 +5,22 @@ import java.util.ArrayList;
 
 public class GerenciadorCias {
 	private ArrayList<CiaAerea> empresas;
-	
-	public GerenciadorCias() {
-		empresas = new ArrayList<>();
-	}
-}
 
+    public GerenciadorCias() {
+        this.empresas = new ArrayList<>();
+    }
+
+    public ArrayList<CiaAerea> listarTodas() {
+        return new ArrayList<>(empresas);
+    }
+
+    public void adicionar(CiaAerea cia1) {
+        empresas.add(cia1);
+    }
+
+    public CiaAerea buscarCodigo(String cod) {
+        for(CiaAerea cia: empresas)
+            if(cia.getCodigo().equals(cod))
+                return cia;
+        return null;
+    }
