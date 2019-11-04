323f1b097fd793cd0cd910f84bf894981a8bac29
MatheusMCosta <37557376+MatheusMCosta@users.noreply.github.com>
2019-11-04 02:52:51 -0300

    Atualizado
    Atualizado

--- MyfLy/GerenciadorAeronaves.java
+++ MyfLy/GerenciadorAeronaves.java
@@ -1,30 +1,46 @@
-
-package myfly;
+package pucrs.myflight.modelo;
 
 import java.util.ArrayList;
-
+import java.util.Collections;
+import java.util.Comparator;
 
 public class GerenciadorAeronaves {
 
     private ArrayList<Aeronave> avioes;
-    
-    public GerenciadorAeronaves(){
-        this.avioes =  new ArrayList<>();
+
+    public GerenciadorAeronaves() {
+        this.avioes = new ArrayList<>();
     }
-    
-    public void adicionar(Aeronave aviao){
+
+    public void adicionar(Aeronave aviao) {
         avioes.add(aviao);
     }
-    
-     public ArrayList<Aeronave> listarTodas() {
+
+    public ArrayList<Aeronave> listarTodas() {
         return new ArrayList<>(avioes);
     }
-    
+
     public Aeronave buscarCodigo(String codigo) {
         for(Aeronave a: avioes)
             if(a.getCodigo().equals(codigo))
                 return a;
         return null;
     }
-        
+
+    public void ordenarDescricao() {
+        /*
+        Collections.sort(avioes);
+        */
+        avioes.sort(Comparator.comparing(Aeronave::getDescricao).reversed());
+    }
+
+    public void ordenarCodigoDescricao() {
+        avioes.sort(Comparator.comparing(Aeronave::getCodigo).
+            thenComparing(Aeronave::getDescricao));
+    }
+
+    public void ordenarCodigo() {
+        avioes.sort( (Aeronave a1, Aeronave a2) ->
+            a1.getCodigo().compareTo(a2.getCodigo()));
+    }
 }
