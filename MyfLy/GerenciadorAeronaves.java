bec3dcef08a7e2a9262e6006fe693e29420ee3ab
Jonatasvangroll <jonatas.vangroll@gmail.com>
2019-11-04 16:47:15 -0300

    Update GerenciadorAeronaves.java

--- MyfLy/GerenciadorAeronaves.java
+++ MyfLy/GerenciadorAeronaves.java
@@ -6,8 +6,6 @@ import java.util.Comparator;
 
 public class GerenciadorAeronaves {
 
-    private ArrayList<Aeronave> avioes;
-
     public GerenciadorAeronaves() {
         this.avioes = new ArrayList<>();
     }
@@ -28,15 +26,24 @@ public class GerenciadorAeronaves {
     }
 
     public void ordenarDescricao() {
-        /*
-        Collections.sort(avioes);
-        */
+        // Usando Comparable<Aeronave> em Aeronave
+        //Collections.sort(avioes);
+
+        // Usando expressão lambda
+        //avioes.sort( (Aeronave a1, Aeronave a2) ->
+        //    a1.getDescricao().compareTo(a2.getDescricao()));
+
+        // Mesma coisa, usando método static da interface Comparator:
+        //avioes.sort(Comparator.comparing(a -> a.getDescricao()));
+
+        // Invertendo o critério de comparação com reversed():
         avioes.sort(Comparator.comparing(Aeronave::getDescricao).reversed());
     }
 
     public void ordenarCodigoDescricao() {
-        avioes.sort(Comparator.comparing(Aeronave::getCodigo).
-            thenComparing(Aeronave::getDescricao));
+       // Ordenando pelo código e desempatando pela descrição
+       avioes.sort(Comparator.comparing(Aeronave::getCodigo).
+               thenComparing(Aeronave::getDescricao));
     }
 
     public void ordenarCodigo() {
